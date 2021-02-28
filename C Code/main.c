3fc3c3bd5624a25d0bcc0019d3244b5f6ddc41ae
Dylan Tocci <dtocci1@umassd.edu>
2021-02-28 16:57:29 -0500

    finished linked list setup, need to adjust functions to support multiple linked lists

--- C Code/main.c
+++ C Code/main.c
@@ -9,24 +9,28 @@ struct Node
 };
 struct Node *head = NULL; // front of the linked-list
 
-void ll_insertFront(short data);
-void ll_insertAtIndex(short index, short data);
-short ll_traverseToIndex(short index);
-void ll_removeFromIndex(short index);
-void ll_replaceIndex(short index, short data);
-short ll_getLength();
+
+void ll_insertFront(short data); // done
+void ll_insertAtIndex(short index, short data); // done
+short ll_traverseToIndex(short index); // done
+void ll_removeFromIndex(short index); // done
+void ll_replaceIndex(short index, short data); // done
+short ll_getLength(); // done
 void ll_merge();
+void ll_printList(); // done
 
 int main() {
-    ll_insertFront(16);
-    ll_insertFront(12);
-    ll_insertFront(10);
+    ll_insertFront(1);
+    ll_insertFront(2);
+    ll_insertFront(3);
 
-    for(short i=0;i<3;i++)
-    {
-        printf(ll_traverseToIndex(i));
-    }
-    return(0);
+    ll_printList();
+
+    ll_replaceIndex(1,5);
+
+    ll_printList();
+    printf("%d",ll_getLength());
+
 }
 
 
@@ -40,7 +44,6 @@ void ll_insertFront(short data) {
         exit(0);
     }
 
-    temp->testData = data;
     if (head == NULL) // Initializing linked list
     {
         head = temp;
@@ -51,42 +54,100 @@ void ll_insertFront(short data) {
         temp->next = head; // point new element to the first element
         head = temp; // point head to new element
     }
+        temp->testData = data;
 }
 
 void ll_insertAtIndex(short index, short data) {
+    struct Node *ptr = NULL;
+    struct Node *oldPtr = NULL;
+    struct Node *temp;
 
+    if(head == NULL)
+        return;
+
+    ptr = head;
+    for (short i=0;i<index-1;i++)
+        ptr = ptr->next;
+
+    oldPtr = ptr->next;
+    temp = (struct Node *) malloc (sizeof(struct Node));
+
+    if (temp == NULL) {
+        printf("ERROR: Memory size-limit reached");
+        exit(0);
+    }
+
+    ptr->next = temp;
+    temp->next = oldPtr;
+    temp->testData = data;
 }
 
 short ll_traverseToIndex(short index) {
-    struct Node *ptr;
+    struct Node *ptr = NULL;
     if (head == NULL)
-        return NULL;
-    else
-    {
-        ptr = head;
-        for(short i=0; i<index; index++)
-            ptr=ptr->next;
-        return ptr->testData;
-    }
+        return 0;
+
+    ptr = head;
+    for(short i=0; i<index; i++)
+        ptr=ptr->next;
+    return ptr->testData;
 }
 
 void ll_removeFromIndex(short index) {
+    struct Node *ptr = NULL;
+    struct Node *nextCell = NULL;
 
+    if (head == NULL)
+        return;
+    
+    // Traverse to cell before removal
+    ptr = head;
+    for(short i=0; i<index-1; i++)
+        ptr = ptr->next;
+    nextCell = ptr->next->next;
+    free(ptr->next);
+    ptr->next = nextCell;
 }
 
 void ll_replaceIndex(short index, short data) {
+    struct Node *ptr = head;
 
+    for(short i=0;i<index;i++)
+        ptr=ptr->next;
+    
+    ptr->testData = data;
 }
 
 short ll_getLength()
 {
-    short length;
+    struct Node *ptr = head; 
+    short length = 1;
 
-
+    while (ptr->next != NULL)
+    {
+        ptr = ptr->next;
+        length++;
+    }
 
     return length;
 }
 
 void ll_merge() {
     //idk if i even need this
+}
+
+void ll_printList() {
+    struct Node *ptr;
+    if (ptr == NULL)
+        return;
+    else
+    {
+        short count = 0;
+        ptr = head;
+        while (ptr != NULL) {
+            printf("[%d]: %d\n", count, ptr->testData);
+            ptr = ptr->next;
+            count++;
+        }
+    }
 }
