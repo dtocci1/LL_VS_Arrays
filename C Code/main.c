cd20a92fe900365d8867b0cba8e85bcdeba6c412
Dylan Tocci <dtocci1@umassd.edu>
2021-02-28 21:41:43 -0500

    Attempted a_insertAtIndex method. Giving me stack smashing error - not sure how this is even overflowing. -broken as of now

--- C Code/main.c
+++ C Code/main.c
@@ -2,10 +2,14 @@
 #include <stdlib.h>
 #include "linkedlist.h" // Header file with ll functions and Node setup
 
+void a_insertAtIndex(short array1[], short len, short index, short data);
+
 int main() {
     struct Node *head1 = NULL; // front of the linked-list 1
     struct Node *head2 = NULL; // front of the linked-list 1
-
+    short test1[5] = {1,2,3,4};
+    a_insertAtIndex(test1,5, 4, 5);
+    //printf("%d", test1[4]);
 }
 
 // Array functions
@@ -16,10 +20,13 @@ int main() {
     * Print
 */
 
-void a_insertAtIndex() {
-
+void a_insertAtIndex(short array1[], short len, short index, short data) {
+    // Shifts elements in array to insert element, assumes there is room in the array
+    for(short i = len-1; i>=index; i--)
+        array1[i+1] = array1[i];
+    array1[index] = data;
 }
-
+/*
 void a_removeFromIndex() {
 
 }
@@ -31,3 +38,4 @@ void a_merge() {
 void a_print() {
     
 }
+*/
