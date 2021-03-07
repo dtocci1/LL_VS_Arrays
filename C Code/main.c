448876f12c3a48086c2de82fa40111f28f12bdf4
Dylan Tocci <dtocci1@umassd.edu>
2021-03-07 17:46:22 -0500

    finished 3/4 array functions, finishing merge soon

--- C Code/main.c
+++ C Code/main.c
@@ -2,40 +2,50 @@
 #include <stdlib.h>
 #include "linkedlist.h" // Header file with ll functions and Node setup
 
-void a_insertAtIndex(short array1[], short len, short index, short data);
+#define TEST_LENGTH 5 // array and linked-list length for test
 
-int main() {
+void a_insertAtIndex(short array1[], short data, short index, short length);
+void a_print(short array[], short length);
+void a_removeFromIndex(short array[], short index, short length);
+
+void main() {
     struct Node *head1 = NULL; // front of the linked-list 1
     struct Node *head2 = NULL; // front of the linked-list 1
-    short test1[5] = {1,2,3,4};
-    a_insertAtIndex(test1,5, 4, 5);
-    //printf("%d", test1[4]);
+    short test1[4] = {1,2,3,4};
+    short test2[4] = {5,6,7,8};
+    short merge[8] = {};
+    
 }
 
 // Array functions
 /*
-    * Insert element
+    * Insert element XXXXXX
     * Remove element
     * Merge
-    * Print
+    * Print XXXXX
 */
 
-void a_insertAtIndex(short array1[], short len, short index, short data) {
+void a_insertAtIndex(short array[], short data, short index, short length) {
     // Shifts elements in array to insert element, assumes there is room in the array
-    for(short i = len-1; i>=index; i--)
-        array1[i+1] = array1[i];
-    array1[index] = data;
+    for(short i = length-2; i>=index; i--)
+        array[i+1] = array[i];
+    array[index] = data;
 }
-/*
-void a_removeFromIndex() {
+
+void a_removeFromIndex(short array[], short index, short length) {
+    // Navigates to index specified then shifts everything to the left one, "deleting" the cell, leaving room at the end of the array
+    for(short i = index; i<length; i++)
+        array[i] = array[i+1];
+    array[length-1] = 0; // 0 represents NULL content as you cannot "remove" from pre-allocated space, this will always be at the end of the array
 
 }
 
-void a_merge() {
-
+void a_merge(short array1[], short length1, short array2[], short length2, short mergedArray[]) {
+    for(short i = 0;)
 }
 
-void a_print() {
-    
+void a_print(short array[], short length) {
+    for (short i = 0; i<length; i++) {
+        printf("%d: %d\n",i,array[i]);
+    }
 }
-*/
